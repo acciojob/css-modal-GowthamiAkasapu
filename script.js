1aae96a536a25898b4ab83155a38a9ac846fdb69
Akasapu Gowthami Durga <137319775+GowthamiAkasapu@users.noreply.github.com>
2025-12-04 20:34:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
-//your JS code here. If required.
 const modal = document.getElementById("myModal");
 const btn = document.getElementById("openModal");
 const closeBtn = document.querySelector(".close-modal");
 
-// Opens the modal by adding the class that sets display: flex
 btn.onclick = function() {
     modal.classList.add("show-modal");
 }
 
-// Closes the modal by removing the class
 closeBtn.onclick = function() {
     modal.classList.remove("show-modal");
 }
 
-// Closes the modal if the click event targets the modal backdrop itself
 window.onclick = function(event) {
     if (event.target === modal) {
         modal.classList.remove("show-modal");
