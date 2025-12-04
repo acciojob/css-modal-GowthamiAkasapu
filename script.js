adca89b8e4b22ccdb39dadf7dc7f0158881d9e96
Akasapu Gowthami Durga <137319775+GowthamiAkasapu@users.noreply.github.com>
2025-12-04 20:21:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const modal = document.getElementById("myModal");
+
+const btn = document.getElementById("openModal");
+
+const closeBtn = document.querySelector(".close-modal");
+
+btn.onclick = function() {
+    modal.classList.add("show-modal");
+}
+
+closeBtn.onclick = function() {
+    modal.classList.remove("show-modal");
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.classList.remove("show-modal");
+    }
+}
